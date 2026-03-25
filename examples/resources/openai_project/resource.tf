5a4343ae1be0ea100b2cd3e87680fdf70e6c4986
farazhm <farazshaikh@hummingbird.co>
2026-03-25 12:49:05 -0700

    polish

--- examples/resources/openai_project/resource.tf
+++ examples/resources/openai_project/resource.tf
@@ -1,3 +1,7 @@
 resource "openai_project" "example" {
   name = "Example Project"
+
+  # Optional: Create the project with the specified data residency region.
+  # Valid values: US, EU, JP, IN, KR, CA, AU, SG
+  geography = "US"
 }
